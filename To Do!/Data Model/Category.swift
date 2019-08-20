89597f343385e23a64115fceb247721f8bffe7f4
Dayana Rios <dayanarios05@gmail.com>
2019-08-19 22:16:31 -0700

    storyboard is set with two table view controllers, realmswift pods installed

---
+++ To Do!/Data Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  To Do!
+//
+//  Created by Dayana on 8/19/19.
+//  Copyright © 2019 Dayana Rios. All rights reserved.
+//
+
+import Foundation
+
+class Category {
+    var name : String = ""
+    
+}
---
+++ To Do!/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  To Do!
+//
+//  Created by Dayana on 8/19/19.
+//  Copyright © 2019 Dayana Rios. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
